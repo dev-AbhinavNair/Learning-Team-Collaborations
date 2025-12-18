4d1f5e262042647699fb5aa10235bd0d1ca956c6
nafihkh <muhammednafih.k.hkottankodan@gmail.com>
2025-12-18 11:49:43 +0530

    DB Connected

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const adminRouter = require("./router/admin.js")
+const adminRouter = require("./router/admin.js");
+const mongoose = require("mongoose")
 
 const app = express();
 
 app.use("/", adminRouter)
+
+mongoose.connection.on("connected", () => console.log("MongoDB connected"));
+mongoose.connection.on("error", err => console.error("MongoDB error:", err));
+mongoose.connection.on("disconnected", () => console.warn("MongoDB disconnected"));
+mongoose.connect("mongodb://localhost:27017/Learning_App")
+
 
 app.listen(1000,()=>{
     console.log("Server is Running:http://localhost:1000/hello")
