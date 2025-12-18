a94d4208b3ee3ade41345e0ed16faae558010e94
nafihkh <muhammednafih.k.hkottankodan@gmail.com>
2025-12-18 11:09:02 +0530

    implementing router system

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const adminRouter = require("./router/admin.js")
+
+const app = express();
+
+app.use("/",)
+
+app.listen(1000,()=>{
+    console.log("Server is Running:https://localhost:1000")
+});
